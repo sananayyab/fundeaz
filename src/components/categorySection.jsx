7b1e12376a7b7d4278d9492931739ce387d19b16
mohdarya <mohsen.a.daryanavard@gmail.com>
2020-11-18 02:37:27 +0400

    changed the group section to show the new style. changed to carrsoul

--- src/components/categorySection.jsx
+++ src/components/categorySection.jsx
@@ -1,18 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, ScrollView, View } from 'react-native';
+import { StyleSheet, ScrollView, View, Dimensions } from 'react-native';
 import { connect } from 'react-redux';
 import CategoryItem from './categoryItem.jsx';
+import Carousel from 'react-native-snap-carousel';
+
 class CategorySection extends React.Component {
+    sliderWidth = Dimensions.get('window').width;
+
+    constructor(props){
+        super(props)
+       
+    }
+    items = ({ item, index }) =>
+    {
+        return (
+            <View style={ { flex: 1, 
+            marginTop: "5%",
+            } }>
+            {item}
+            </View>
+        );
+    }
     render() {
         const styles = StyleSheet.create({
             container: {
-                marginTop: 20,
+                marginTop: 0,
                 marginBottom: '4%',
                 marginLeft: '3%',
                 marginRight: '3%',
                 borderRadius: 10,
-                backgroundColor: 'rgba(128,128,128,0.4)',
+                flexDirection: 'row',
+                backgroundColor: '#8D8D92',
                 flex: 1,
             },
             outterContainer: {
@@ -20,9 +39,14 @@ class CategorySection extends React.Component {
         })
         return (
             <View style={styles.container}>
-                <ScrollView  >
-                {this.props.stuff.Groups.map((item) => ( <CategoryItem key={item.id} name={item.name} navigation={this.props.navigation}/>))}
-                </ScrollView>
+               
+               <Carousel
+                
+                    data={ this.props.stuff.Groups.map((item) => ( <CategoryItem key={item.id} name={item.name} navigation={this.props.navigation}/>))}
+                    renderItem={ this.items }
+                    sliderWidth={ this.sliderWidth }
+                    itemWidth={ this.sliderWidth / 2.5}
+                />
             </View>
         );
     }
