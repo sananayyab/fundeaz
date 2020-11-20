cba528a92dbfc78b4da359bb1431ac9a5ebe2721
mohdarya <mohsen.a.daryanavard@gmail.com>
2020-11-20 22:29:28 +0400

    updated the group page

--- src/components/transactionSection.jsx
+++ src/components/transactionSection.jsx
@@ -1,40 +1,89 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, ScrollView, View } from 'react-native';
-import TransactionItem from './transactionItem.jsx';
-class TransactionSection extends React.Component {
-    render() {
+import { StyleSheet, View, ScrollView, TouchableOpacity } from 'react-native';
+
+import TransactionItem from './transactionItem';
+class TransactionSection  extends React.Component
+{
+
+    render()
+    {
         const styles = StyleSheet.create({
             container: {
-                marginTop: 20,
-                marginBottom: '4%',
-                marginLeft: '3%',
-                marginRight: '3%',
-                borderRadius: 10,
-                backgroundColor: 'rgba(128,128,128,0.4)',
                 flex: 1,
+
+
+          
+                marginLeft: '2%',
+                marginRight: '2%',
+                borderTopRightRadius: 15,
+                borderTopLeftRadius: 15,
+                borderBottomLeftRadius: 15,
+             
+                borderBottomRightRadius: 15,
+              
+                backgroundColor: '#8D8D92',
+    
+
             },
-            outterContainer: {
+            TransactionContainer: {
+              
+              
+                flex: 1,
+              
+               
+               
+                
             },
+       
+            TransactionButton: {
+                width: '100%',
+           
+                borderTopRightRadius: 15,
+                borderTopLeftRadius: 15,
+                height: '8%',
+                zIndex: 1,
+                position: 'absolute',
+            
+                
+              
+            },
+            lines: {
+                alignSelf: 'center',
+                marginTop: '1.5%',
+                height: '50%',
+                borderRadius: 30,
+                backgroundColor: '#C8C8C8',
+                width: '30%',
+            }
+
+
         })
+
+
         return (
-            <View style={styles.container}>
-                <ScrollView  >
-                    <TransactionItem />
-                    <TransactionItem />
-                    <TransactionItem />
-                    <TransactionItem />
-                    <TransactionItem />
-                    <TransactionItem />
-                    <TransactionItem />
-                    <TransactionItem />
-                    <TransactionItem />
-                    <TransactionItem />
-                    <TransactionItem />
-                    <TransactionItem />
-                </ScrollView>
+            <View style={ styles.container } >
+                    <TouchableOpacity style={styles.TransactionButton}>
+                        <View style={styles.lines} />
+                    </TouchableOpacity>
+                   
+            <ScrollView style={styles.TransactionContainer} showsVerticalScrollIndicator ={false}
+            showsHorizontalScrollIndicator={false}>
+                <View style={{height: '5%'}}/>
+               <TransactionItem />
+               <TransactionItem/>
+               <TransactionItem/>
+                 <TransactionItem/>
+               <TransactionItem/>
+               <TransactionItem/>
+               <View style={{height: 51}}/>
+               
+            </ScrollView>
             </View>
         );
     }
 }
+
+
+
 export default TransactionSection
--- src/containers/groupPage.jsx
+++ src/containers/groupPage.jsx
@@ -1,28 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ToastAndroid, TouchableOpacity } from 'react-native';
 import { registerRootComponent } from 'expo';
 import TopBar from '../components/topBarHome.jsx';
-import InformationSection from '../components/informationSection.jsx'; 
+import CategorySection from '../components/categorySection.jsx';
 import BottomBar from '../components/bottomBar.jsx';
-
-
-class GroupPage extends React.Component
-{
-
-
-
-    constructor(props)
-    {
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import SpendingSection from '../components/transactionSection.jsx';
+import { Feather } from '@expo/vector-icons';
+class HomePage extends React.Component {
+    constructor(props) {
         super(props)
-        
-   
     }
-
- 
-    render()
-    {
-        
+    render() {
         const styles = StyleSheet.create({
             container: {
                 flex: 1,
@@ -30,32 +21,60 @@ class GroupPage extends React.Component
                 backgroundColor: 'white',
             },
             topContainer: {
-                flex: 3
+                flex: 4
             },
-            lowerContainer: {
-                flex: 10,
+            categoryContainer: {
+                flex: 8,
+                flexDirection: 'column'
+            },
+            spendingContainer: {
+                flex: 12,
+                flexDirection: 'column'
             },
             bottomBar:
             {
-                flex: 0.8
+                paddingTop: '4%',
+                paddingBottom: '3%',
+                flex: 1
+            },
+            categoryButton: {
+                marginLeft: '2%',
+                marginRight: '2%',
+                borderTopRightRadius: 15,
+                borderTopLeftRadius: 15,
+                height: '12%',
+                backgroundColor: '#8D8D92',
+            },
+
+            lines: {
+                alignSelf: 'center',
+                marginTop: '1.5%',
+                height: '50%',
+                borderRadius: 30,
+                backgroundColor: '#C8C8C8',
+                width: '30%',
             }
         });
         return (
-            <View style={ styles.container }>
-                <View style={ styles.topContainer }>
-                    
-                    <TopBar name={this.props.route.params.name}/>
+            <View style={styles.container}>
+                <View style={styles.topContainer}>
+                    <TopBar navigation={this.props.navigation} />
                 </View>
-                <View style={ styles.lowerContainer }>
-                    <InformationSection style={ { flex: 1 } }  />
+                <View style={styles.categoryContainer}>
+                    <TouchableOpacity style={styles.categoryButton}>
+                        <View style={styles.lines} />
+                    </TouchableOpacity>
+                    <CategorySection section={"group"} style={{ flex: 1 }} navigation={this.props.navigation} />
                 </View>
-                <View style={ styles.bottomBar } >
+                <View style={styles.spendingContainer}>
+                
+                    <SpendingSection />
+                </View>
+                <View style={styles.bottomBar}>
                     <BottomBar />
                 </View>
             </View>
         );
     }
 }
-
-
-export default GroupPage;
+export default HomePage;
