5eeb72a966bfeea22af2a044ec1ed8804fdb3ba1
mohdarya <mohsen.a.daryanavard@gmail.com>
2020-11-22 23:56:34 +0400

    disabled center view for category section

--- src/components/categorySection.jsx
+++ src/components/categorySection.jsx
@@ -47,7 +47,10 @@ class CategorySection extends React.Component {
             <View style={styles.container}>
                
                <Carousel
-                
+               enableSnap={false}
+               inactiveSlideOpacity={1}
+               inactiveSlideScale={1}
+                activeSlideAlignment={'start'}
                     data={ this.props.stuff.Groups.map((item) => ( <CategoryItem key={item.id} name={item.name} navigation={this.props.navigation}/>))}
                     renderItem={ this.items }
                     sliderWidth={ this.sliderWidth  }
