ed46ad2b46b090ddfd3c0f7e00b265cb7a3b3dc4
mohdarya <mohsen.a.daryanavard@gmail.com>
2020-12-10 17:02:54 +0400

    added action for changing balance

---
+++ src/action/fundActions.jsx
@@ -0,0 +1,63 @@
+export function addTotalAvailable(amount) {
+    return {
+        type: 'ADD_TOTAL_AVAILABLE',
+        amount,
+    
+    }
+}
+export function removeTotalAvailable(amount) {
+    return {
+        type: 'REMOVE_TOTAL_AVAILABLE',
+        amount,
+
+    }
+}
+
+export function allocateToCategory(amount, groupID, categoryID) {
+    return {
+        type: 'ALLOCATE_TO_CATEGORY',
+        groupID,
+        categoryID,
+        amount,
+
+    }
+}
+
+export function deallocateCategory(amount, groupID, categoryID) {
+    return {
+        type: 'DEALLOCATE_CATEGORY',
+        groupID,
+        categoryID,
+        amount,
+
+    }
+}
+
+
+export function spendCategory(amount, groupID, categoryID) {
+    return {
+        type: 'SPEND_CATEGORY',
+        groupID,
+        categoryID,
+        amount,
+
+    }
+}
+
+export function removeSpendCategory(amount, groupID, categoryID) {
+    return {
+        type: 'REMOVE_SPEND_CATEGORY',
+        groupID,
+        categoryID,
+        amount,
+
+    }
+}
+
+
+
+
+
+
+
+
