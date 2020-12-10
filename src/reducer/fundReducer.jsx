cec1e0d77f6f6a69d671f75c5085c00fbf1d2878
mohdarya <mohsen.a.daryanavard@gmail.com>
2020-12-10 17:05:13 +0400

    added reducer for balance managment

---
+++ src/reducer/fundReducer.jsx
@@ -0,0 +1,120 @@
+/*
+possible fields: 
+
+total: 
+group: 
+category:
+*/
+
+const initialState = {
+    available: 0,
+    unallocated: 0,
+    groups: {
+
+    }
+}
+export function groupReducer(state = initialState, action) {
+    switch (action.type) {
+        case 'ADD_TOTAL_AVAILABLE':
+            return {
+                ...state,
+                available: state.available + action.amount,
+                unallocated: state.unallocated + action.amount,
+            }
+        case 'REMOVE_TOTAL_AVAILABLE':
+            return {
+                ...state,
+                available: state.available - action.amount,
+                unallocated: state.unallocated - action.amount,
+            }
+        case 'ALLOCATE_TO_CATEGORY':
+            return {
+                ...state,
+                unallocated: state.unallocated - action.amount,
+                groups: {
+                    ...state.groups,
+                    [action.groupID]: {
+                        ...state.groups[action.groupID],
+                        available: state.groups[action.groupID].available + amount,
+                        allocated: state.groups[action.groupID].allocated + amount,
+                        [action.categoryID]: {
+                            ...state.groups[action.groupID].categories[action.categoryID],
+                            available: state.groups[action.groupID].categories[action.categoryID].available + amount,
+                            allocated: state.groups[action.groupID].categories[action.categoryID].allocated + amount,
+
+                        }
+                    }
+
+                }
+            }
+            case 'DEALLOCATE_CATEGORY':
+                allocatedToGroup = state.groups[action.groupID].allocated - amount
+                allocatedToCategory = state.groups[action.groupID].categories[action.categoryID].allocated - amount
+                if(allocatedToGroup < 0)
+                {
+                    allocatedToGroup = 0
+                }
+                if(allocatedToCategory < 0 )
+                {
+                    allocatedToCategory = 0
+                }
+                return {
+                    ...state,
+                    unallocated: state.unallocated + action.amount,
+                    groups: {
+                        ...state.groups,
+                        [action.groupID]: {
+                            ...state.groups[action.groupID],
+                            available: state.groups[action.groupID].available - amount,
+                            allocated: allocatedToGroup,
+                            [action.categoryID]: {
+                                ...state.groups[action.groupID].categories[action.categoryID],
+                                available: state.groups[action.groupID].categories[action.categoryID].available - amount,
+                                allocated: allocatedToCategory,
+    
+                            }
+                        }
+    
+                    }
+                }
+        case 'SPEND_CATEGORY':
+            return {
+                ...state,
+                available: state.available - action.amount,
+                groups: {
+                    ...state.groups,
+                    [action.groupID]: {
+                        ...state.groups[action.groupID],
+                        available: state.groups[action.groupID].available - amount,
+                        [action.categoryID]: {
+                            ...state.groups[action.groupID].categories[action.categoryID],
+                            available: state.groups[action.groupID].categories[action.categoryID].available - amount,
+
+                        }
+                    }
+
+                }
+            }
+        case 'REMOVE_SPEND_CATEGORY':
+            return {
+                ...state,
+                available: state.available + action.amount,
+                groups: {
+                    ...state.groups,
+                    [action.groupID]: {
+                        ...state.groups[action.groupID],
+                        available: state.groups[action.groupID].available + amount,
+                        [action.categoryID]: {
+                            ...state.groups[action.groupID].categories[action.categoryID],
+                            available: state.groups[action.groupID].categories[action.categoryID].available + amount,
+
+                        }
+                    }
+
+                }
+            }
+        default:
+            return state
+
+    }
+}
