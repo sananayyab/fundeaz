1678a25670b5ddfce734ef4fbcfaa82f43837db3
mohdarya <mohsen.a.daryanavard@gmail.com>
2020-12-10 17:03:29 +0400

    added action for group managment

---
+++ src/action/groupActions.jsx
@@ -0,0 +1,47 @@
+export function addGroup(data){
+    return{
+    type: 'ADD_GROUP',
+    data,
+    }
+}
+
+export function removeGroup(groupID){
+    return{
+    type: 'REMOVE_GROUP',
+    groupID,
+    }
+}
+
+export function updateGroup(data, groupID){
+    return{
+    type: 'UPDATE_GROUP',
+    groupID,
+    data,
+    }
+}
+
+
+export function addCategory(data, groupID){
+    return{
+    type: 'ADD_CATEGORY',
+    groupID,
+    data,
+    }
+}
+
+export function removeCategory(categoryID,groupID){
+    return{
+    type: 'REMOVE_CATEGORY',
+    categoryID,
+    groupID,
+    }
+}
+
+export function updateCategory(data, categoryID,groupID){
+    return{
+    type: 'UPDATE_CATEGORY',
+    categoryID,
+    groupID,
+    data,
+    }
+}
