906e352907d1436176935d4ace5662b150a5fcc1
mohdarya <mohsen.a.daryanavard@gmail.com>
2020-12-10 17:03:53 +0400

    added action for managing transactions

---
+++ src/action/transactionActions.jsx
@@ -0,0 +1,21 @@
+export function addTransaction(data){
+    return{
+    type: 'ADD_TRANSACTION',
+    data,
+    }
+}
+
+export function removeTransaction(id){
+    return{
+    type: 'REMOVE_TRANSACTION',
+    id,
+    }
+}
+
+export function updateTransaction(data, id){
+    return{
+    type: 'UPDATE_TRANSACTION',
+    id,
+    data,
+    }
+}
