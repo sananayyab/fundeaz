3319fb7586e92d9d56ad4be735e4db52c691705c
mohdarya <mohsen.a.daryanavard@gmail.com>
2021-04-05 20:58:22 +0400

    fixed fund change not appearing

--- src/components/topBarItem.jsx
+++ src/components/topBarItem.jsx
@@ -1,23 +1,17 @@
-
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { connect } from 'react-redux';
-
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         marginLeft: '2%',
         marginRight: '2%',
-
         borderRadius: 10,
         flexDirection: 'row',
-
     },
     innerContainerAmount: {
         flex: 2.5,
-
         height: '70%',
-
         borderRadius: 5,
         flexDirection: 'column',
         justifyContent: 'center',
@@ -62,37 +56,27 @@ const styles = StyleSheet.create({
         color: 'white',
     },
     textText: {
-
         alignSelf: 'center',
         textAlign: 'center',
         fontSize: 17,
         color: 'white',
     }
 })
-var amount = null;
 
 function TopBarItem(props) {
-   
     const [toUse, setTouse] = useState()
     const [text, setText] = useState()
-
-
+    const [amount, setAmount] = useState()
     useEffect(() => {
         if (props.type === 'unallocated') {
-
-
             setTouse(styles.innerContainerTextAllocation)
             setText('Unallocated')
-
-
-            amount = props.unallocated
+            setAmount(props.unallocated)
         }
         else if (props.type === 'allocated') {
-
             setTouse(styles.innerContainerTextAllocation)
             setText('Allocated')
-
-            amount = props.groups[props.groupID].allocated
+            setAmount(props.groups[props.groupID].allocated)
         }
         else if (props.type === 'amount') {
             if (props.groupID === null) {
@@ -100,50 +84,36 @@ function TopBarItem(props) {
             } else {
                 var value = props.groups[props.groupID].available
             }
-
             if (value >= 0) {
                 var toUse = styles.innerContainerTextPositive
             }
             else if (value < 0) {
                 var toUse = styles.innerContainerTextNegative
             }
-
             setTouse(toUse)
             setText('Available')
-            amount = value
+            setAmount(value)
         }
-
-    })
-
-
-
-
-
-
+    }, [props.fund])
     return (
         <View style={styles.container}>
             <View style={styles.innerContainerAmount}>
-
                 <Text style={styles.textAmount} >{amount}</Text>
             </View>
             <View style={toUse}>
                 <Text style={styles.textText}>{text}</Text>
-
             </View>
         </View>
     );
 }
-
-
-
 const mapStateToProps = (state) => {
     const { fund } = state
     return {
         available: fund.available,
         allocated: fund.allocated,
         unallocated: fund.unallocated,
-        groups: fund.groups
+        groups: fund.groups, 
+        fund: fund
     }
 };
 export default connect(mapStateToProps)(TopBarItem)
-
