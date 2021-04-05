17edeedc24463423639e48a85bc07594747f3fc5
mohdarya <mohsen.a.daryanavard@gmail.com>
2021-04-05 19:07:40 +0400

    changed to functional component

--- src/components/transactionInputFieldDate.jsx
+++ src/components/transactionInputFieldDate.jsx
@@ -1,135 +1,123 @@
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, View, ScrollView, TouchableOpacity, TouchableWithoutFeedbackBase, Text, Platform } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import { connect } from 'react-redux';
 
-class TransactionInputFieldDate extends React.Component {
+function TransactionInputFieldDate (props) {
+    const styles = StyleSheet.create({
+        container: {
+            top: 10,
+            height: 80,
 
-    constructor(props) {
-        super(props)
-        var date;
-        if(this.props.value !== '')
+        },
+        fieldNameContainer: {
+            position: 'absolute',
+            height: '55%',
+            zIndex: 0,
+            borderRadius: 15,
+            top: '10%',
+            width: '35%',
+            marginLeft: '3%',
+            backgroundColor: '#00487C'
+        },
+        textFieldContainer: {
+            marginTop: '10%',
+
+            borderRadius: 15,
+            zIndex: 2,
+            height: '55%',
+            marginRight: '3%',
+
+            marginLeft: '20%',
+            backgroundColor: '#1D2D44'
+        },
+        fieldNameText: {
+
+            paddingRight: '6%',
+
+            alignSelf: 'center',
+
+            fontSize: 19,
+            color: 'white',
+
+        },
+        textInput: {
+            top: 3,
+            flex: 1,
+            color: 'white',
+            textAlign: 'center',
+            fontSize: 20,
+            marginRight: 15,
+            marginLeft: 15,
+        }
+
+
+    })
+  
+
+
+    const [showDatePicker, setShowDatePicker] = useState(false);
+    const [date, setDate] = useState()
+    useEffect(() => {
+  
+        var initialDate;
+        if(props.value !== '')
         {
-            date = new Date (this.props.value)
+            initialDate = new Date (props.value)
         }
         else{
-            date = new Date()
+            initialDate = new Date()
         }
-        this.onChange = this.onChange.bind(this)
-        this.showDatepicker = this.showDatepicker.bind(this)
-        this.state = {
-            date: date,
-            show: false,
-
-        }
-        this.props.data ( {
+        setDate(initialDate)
+        props.data ( {
           
-            date: this.state.date
+            date: date
         })
-    }
+    }, [])
 
-    onChange = (event, selectedDate) => {
-        const currentDate = selectedDate || this.state.date;
+   const onChange = (event, selectedDate) => {
+        const currentDate = selectedDate || date;
        
-        this.setState({
-            show: Platform.OS=== 'ios',
-            date: currentDate
-        })
-
-        this.props.data ( {
+        setDate(currentDate)
+        setShowDatePicker(Platform.OS=== 'ios')
+        props.data ( {
           
-            date: this.state.date
+            date: date
         })
     };
 
 
 
-    showDatepicker = () => {
-       this.setState({
-           show: true
-       })
-     
-    };
 
 
 
-    render() {
 
-
-        const styles = StyleSheet.create({
-            container: {
-                top: 10,
-                height: 80,
-
-            },
-            fieldNameContainer: {
-                position: 'absolute',
-                height: '55%',
-                zIndex: 0,
-                borderRadius: 15,
-                top: '10%',
-                width: '35%',
-                marginLeft: '3%',
-                backgroundColor: '#00487C'
-            },
-            textFieldContainer: {
-                marginTop: '10%',
-
-                borderRadius: 15,
-                zIndex: 2,
-                height: '55%',
-                marginRight: '3%',
-
-                marginLeft: '20%',
-                backgroundColor: '#1D2D44'
-            },
-            fieldNameText: {
-
-                paddingRight: '6%',
-
-                alignSelf: 'center',
-
-                fontSize: 19,
-                color: 'white',
-
-            },
-            textInput: {
-                top: 3,
-                flex: 1,
-                color: 'white',
-                textAlign: 'center',
-                fontSize: 20,
-                marginRight: 15,
-                marginLeft: 15,
-            }
-
-
-        })
+    
         return (
             <View style={styles.container} >
                 <View style={styles.fieldNameContainer}>
                     <Text style={styles.fieldNameText}>
-                        {this.props.fieldName}
+                        {props.fieldName}
                     </Text>
                 </View>
-                <TouchableOpacity activeOpacity={1} style={styles.textFieldContainer} onPress={this.showDatepicker} >
+                <TouchableOpacity activeOpacity={1} style={styles.textFieldContainer} onPress={() => {setShowDatePicker(true)}} >
 
                     <Text style={styles.textInput}>
-                    {this.state.date.toDateString()}
+                    {date.toDateString()}
                     </Text>
                     </TouchableOpacity>
-                {this.state.show && (
+                {showDatePicker && (
                 
                     <DateTimePicker
                         testID="dateTimePicker"
                         style={{flex: 1, backgroundColor: 'white'}}
-                        value={this.state.date}
+                        value={date}
                         mode={'date'}
                         is24Hour={true}
                         display="default"
-                        onChange={this.onChange}
+                        onChange={onChange}
                     />
                 )}
 
@@ -137,7 +125,7 @@ class TransactionInputFieldDate extends React.Component {
             </View>
         );
     }
-}
+
 export default TransactionInputFieldDate
 
 
