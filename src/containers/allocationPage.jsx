4d811290a4f7f7ebd526d439b675c6d645dffb3f
mohdarya <mohsen.a.daryanavard@gmail.com>
2021-04-05 19:16:11 +0400

    changed to functional component

--- src/containers/allocationPage.jsx
+++ src/containers/allocationPage.jsx
@@ -4,15 +4,9 @@ import { useNavigation } from '@react-navigation/native';
 import { connect } from 'react-redux';
 import AllocationGroup from '../components/allocationGroup.jsx'
 import TopBarItem from '../components/topBarItem.jsx';
-class AllocationPage extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            type: 'unallocated',
-            unallocated: this.props.unallocated
-        }
-    }
-    render() {
+function AllocationPage (props) {
+
+    
         const styles = StyleSheet.create({
             container: {
                 flex: 1,
@@ -28,15 +22,15 @@ class AllocationPage extends React.Component {
         return (
             <View style={styles.container}>
                 <View style={styles.topBarContainer}>
-                    <TopBarItem type={this.state.type} value={this.state.unallocated} style={{ flex: 1 }} />
+                    <TopBarItem type={'unallocated'} value={props.unallocated} style={{ flex: 1 }} />
                 </View>
                 <ScrollView style={{flex: 1}} showsVerticalScrollIndicator ={false}
   showsHorizontalScrollIndicator={false}>
-                    {Object.entries(this.props.groups).map(([key, value]) => < AllocationGroup key={key} groupID={key} />)}
+                    {Object.entries(props.groups).map(([key, value]) => < AllocationGroup key={key} groupID={key} />)}
                 </ScrollView>
             </View>)
     }
-}
+
 const mapStateToProps = (state) => {
     const { groupData, fund } = state
     return {
