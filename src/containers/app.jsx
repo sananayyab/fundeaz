6886fc7cf91aa3aa48fb3e8365e1067d6f097ca6
mohdarya <mohsen.a.daryanavard@gmail.com>
2021-04-05 19:17:05 +0400

    changed to functional component

--- src/containers/app.jsx
+++ src/containers/app.jsx
@@ -12,62 +12,52 @@ import settingPage from './settingPage.jsx';
 import TransactionListPage from './transactionListPage.jsx';
 import AllocationPage from './allocationPage.jsx';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator , CardStyleInterpolators} from '@react-navigation/stack';
-import { createStore} from 'redux'
-import {Provider} from 'react-redux'
-import {store, persistor} from '../reducer/store'
+import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import { store, persistor } from '../reducer/store'
 import { PersistGate } from 'redux-persist/integration/react';
-class App extends React.Component
-{
-   
+function App(props) {
 
-    constructor(props)
-    {
-        super(props)
-       //persistor.purge()
-    }
-    render()
-    {
-       
-        const Stack = createStackNavigator();
-        return (
-            
-            <Provider store={store}>
 
-                <PersistGate loading={null} persistor={persistor}>
-            <NavigationContainer>
-                
-                <Stack.Navigator initialRouteName="HomePage" 
-                 screenOptions={{headerShown: false, 
-                    gestureEnabled: true,
-                    gestureDirection: "horizontal",
-                   cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
-                    
-                }} 
-              >
-                    <Stack.Screen name="HomePage" component={ HomePage } />
-                    <Stack.Screen name="GroupPage" component={ GroupPage} />
-                    <Stack.Screen name="CategoryList" component={ CategoryListPage} />
-                    <Stack.Screen name="GroupList" component={ GroupListPage} />
-                    <Stack.Screen name="TransactionList" component={ TransactionListPage} />
-                    <Stack.Screen name="TransactionInput" component={ TransactionInput } />
-                    <Stack.Screen name="TransactionEdit" component={ TransactionEdit } />
-                    <Stack.Screen name="AllocationPage" component={ AllocationPage } />
-                    <Stack.Screen name="SettingPage" component={ settingPage } />
-                </Stack.Navigator>
+
+
+    //persistor.purge()
+
+    const Stack = createStackNavigator();
+    return (
+
+        <Provider store={store}>
+
+            <PersistGate loading={null} persistor={persistor}>
+                <NavigationContainer>
+
+                    <Stack.Navigator initialRouteName="HomePage"
+                        screenOptions={{
+                            headerShown: false,
+                            gestureEnabled: true,
+                            gestureDirection: "horizontal",
+                            cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+
+                        }}
+                    >
+                        <Stack.Screen name="HomePage" component={HomePage} />
+                        <Stack.Screen name="GroupPage" component={GroupPage} />
+                        <Stack.Screen name="CategoryList" component={CategoryListPage} />
+                        <Stack.Screen name="GroupList" component={GroupListPage} />
+                        <Stack.Screen name="TransactionList" component={TransactionListPage} />
+                        <Stack.Screen name="TransactionInput" component={TransactionInput} />
+                        <Stack.Screen name="TransactionEdit" component={TransactionEdit} />
+                        <Stack.Screen name="AllocationPage" component={AllocationPage} />
+                        <Stack.Screen name="SettingPage" component={settingPage} />
+                    </Stack.Navigator>
                 </NavigationContainer>
-                </PersistGate>
-                </Provider>
-        );
-    }
+            </PersistGate>
+        </Provider>
+    );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        
-    },
-});
+
+
 
 export default App;
