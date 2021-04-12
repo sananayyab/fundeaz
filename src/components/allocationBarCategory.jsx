396941896ca1725c23d7ccc775904b9f725fda87
mohdarya <mohsen.a.daryanavard@gmail.com>
2021-04-12 21:13:37 +0400

    made it so you can add and deduct with swiping

--- src/components/allocationBarCategory.jsx
+++ src/components/allocationBarCategory.jsx
@@ -1,16 +1,24 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, ToastAndroid, TextInput } from 'react-native';
+import React, { createRef, useRef, useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, ToastAndroid, TextInput , Animated} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { connect } from 'react-redux';
 import { allocateToCategory, deallocateCategory } from '../action/fundActions.jsx'
-import parseErrorStack from 'react-native/Libraries/Core/Devtools/parseErrorStack';
-function AllocationBarCategory(props) {
 
-    const [amount, setAmount] = useState(props.fundAllocated)
+import { FlingGestureHandler, State, Directions } from 'react-native-gesture-handler';
+
+
+
+var mode; 
+var oldAmount;
+var amount;
+function AllocationBarCategory(props) {
+    amount = props.fundAllocated
+    const position = useRef(new Animated.Value(0)).current;
 
     const styles = StyleSheet.create({
         container: {
             height: 45,
+            width: '97%',
             marginLeft: 5,
             marginRight: 5,
             marginTop: 5,
@@ -20,6 +28,8 @@ function AllocationBarCategory(props) {
             backgroundColor: '#1D2D44',
             justifyContent: 'center',
             alignItems: 'center',
+            position: 'absolute',
+            transform: [{translateX: position}]
         },
         innerContainerText: {
             flex: 2.5,
@@ -62,7 +72,7 @@ function AllocationBarCategory(props) {
         },
         textAmount: {
            top: '5%',
-            height: '150%',
+            height: '200%',
             width: '100%',
         
             textAlign: 'center',
@@ -71,26 +81,92 @@ function AllocationBarCategory(props) {
             color: 'white',
         }
     })
+    const [styleToUse, setStyle] = useState(((parseInt(props.fundAllocated) >= 0) ? styles.innerContainerTextPositive : styles.innerContainerTextNegative))
 
+const textFieldRef = createRef()
+
+var beginX
+function handleState({nativeEvent}){
+    if(nativeEvent.state === State.BEGAN)
+    {   
+       
+        beginX = nativeEvent.absoluteX
+        
+    }
+    if(nativeEvent.state === State.END)
+    {
+        if(nativeEvent.absoluteX > beginX )
+        {
+            Animated.sequence([Animated.timing(position,{ toValue: 50, duration: 100,useNativeDriver: true}), Animated.timing(position,{ toValue: 0,duration: 100 ,useNativeDriver : true})]).start()
+            setStyle( styles.innerContainerTextPositive )
+            oldAmount = amount
+            amount = 0
+            mode = 'add'
+            textFieldRef.current.focus()
+        }
+        else{
+            Animated.sequence([Animated.timing(position,{ toValue: -50, duration: 100,useNativeDriver: true}), Animated.timing(position,{ toValue: 0,duration: 100 ,useNativeDriver : true})]).start()
+            setStyle(styles.innerContainerTextNegative)
+            oldAmount = amount
+            amount = 0
+            mode = 'deduct'
+            textFieldRef.current.focus()
+        }
+       
+    }
+    if(nativeEvent.state === State.CANCELLED)
+    {
+        if(nativeEvent.absoluteX > beginX )
+        {
+            Animated.sequence([Animated.timing(position,{ toValue: 50, duration: 100,useNativeDriver: true}), Animated.timing(position,{ toValue: 0,duration: 100 ,useNativeDriver : true})]).start()
+            setStyle( styles.innerContainerTextPositive )
+            oldAmount = amount
+            amount = 0
+            mode = 'add'
+            textFieldRef.current.focus()
+        }
+        else{
+            Animated.sequence([Animated.timing(position,{ toValue: -50, duration: 100,useNativeDriver: true}), Animated.timing(position,{ toValue: 0,duration: 100 ,useNativeDriver : true})]).start()
+            setStyle(styles.innerContainerTextNegative)
+            oldAmount = amount
+            amount = 0
+            mode = 'deduct'
+            textFieldRef.current.focus()
+        }
+ 
+    }
+}
+   
+     
         
         return (
-            <View style={styles.container}>
+            <FlingGestureHandler  direction={Directions.RIGHT | Directions.LEFT }
+        
+            onHandlerStateChange={handleState}> 
+            <View style={{height: 50,
+        width: '100%'}}>
+            <Animated.View style={styles.container}>
                 <View style={styles.innerContainerText}>
                     <Text style={styles.textText} >{props.name}</Text>
                 </View>
-                <View style={((parseInt(props.fundAllocated) >= 0) ? styles.innerContainerTextPositive : styles.innerContainerTextNegative)}>
+                <View  style={styleToUse}>
                     <TextInput
+                    ref={textFieldRef}
                         selectTextOnFocus={true}
                         onEndEditing={(event) => {
-                            if (event.nativeEvent.text > amount) {
-                                props.allocate((parseInt(event.nativeEvent.text) - parseInt( amount)))
-                                if(amount === 0)
-                                {
-                                setAmount(parseInt(event.nativeEvent.text))
-                                }
+                            if (mode === 'add') {
+                                props.allocate((parseInt(event.nativeEvent.text) ))
+                              
+                                amount = parseInt(event.nativeEvent.text) + parseInt( oldAmount)
+                           
+                                setStyle( styles.innerContainerTextPositive )
+                                
                             }
-                            else if (event.nativeEvent.text < amount) {
-                                props.deallocate(amount - parseInt( event.nativeEvent.text))
+                            else if (mode === 'deduct') {
+                                props.deallocate( parseInt( event.nativeEvent.text))
+                                amount =  oldAmount - parseInt( event.nativeEvent.text)
+                           
+                                setStyle( styles.innerContainerTextPositive )
                                 
                             }
                         }}
@@ -102,7 +178,9 @@ function AllocationBarCategory(props) {
 
 
                 </View>
+            </Animated.View>
             </View>
+            </FlingGestureHandler>
         );
     }
 
