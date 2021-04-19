3d716951b57db244802810215709daab6420e413
mohdarya <mohsen.a.daryanavard@gmail.com>
2021-04-19 23:41:34 +0400

    changed the look of it

--- src/components/transactionCategoryItem.jsx
+++ src/components/transactionCategoryItem.jsx
@@ -7,48 +7,43 @@ function TransactionCategoryListItem (props) {
         const styles = StyleSheet.create({
             container: {
                 flex: 1,
-                marginLeft: 5,
-                marginRight: 5,
-                marginTop: 5,
-                marginBottom: 5,
+                margin: 5,
                 borderRadius: 10,
                 flexDirection: 'row',
-                opacity: 1,
+                backgroundColor: '#385782',
+                justifyContent: 'center',
+                alignItems: 'center',
             },
             innerContainerText: {
                 flex: 3.3,
-            
                 height: '100%',
                 borderRadius: 5,
                 flexDirection: 'column',
                 justifyContent: 'center',
                 alignItems: 'flex-start',
-                backgroundColor: '#1D2D44',
+                backgroundColor: '#385782',
             },
             innerContainerTextPositive: {
-                flex: 1.3,
-                top: 6,
-                height: '75%',
-                borderBottomRightRadius: 5,
-                borderTopRightRadius: 5,
+                flex: 1.45,
+                marginRight: '3%',
+                height: '70%',
+                borderRadius: 6,
                 flexDirection: 'column',
                 justifyContent: 'center',
                 alignItems: 'center',
                 backgroundColor: '#05845D',
             },
             innerContainerTextNegative: {
-                flex: 1.3,
-                top: 6,
-                height: '75%',
-                borderBottomRightRadius: 5,
-                borderTopRightRadius: 5,
+                flex: 1.45,
+                marginRight: '3%',
+                height: '70%',
+                borderRadius: 6,
                 flexDirection: 'column',
                 justifyContent: 'center',
                 alignItems: 'center',
                 backgroundColor: '#85041C',
             },
             innerContainerAmount: {
-                
             },
             textText: {
                 paddingLeft: 0,
@@ -58,10 +53,9 @@ function TransactionCategoryListItem (props) {
                 marginLeft: '5%',
             },
             textAmount: {
-              
                 fontSize: 20,
                 color: 'white',
-            }
+            },
         })
         return (
            <TouchableOpacity activeOpacity={1} onPress={() => {props.press(props.name, props.amount, props.groupID,props.categoryID)}} style={styles.container}>
