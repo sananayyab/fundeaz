55ff44ed854c545ceed8174ead7092bdedc23354
mohdarya <mohsen.a.daryanavard@gmail.com>
2021-04-20 21:17:23 +0400

    created barebone function

---
+++ src/containers/fundOverviewPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { StyleSheet, Text, View, ToastAndroid, TouchableOpacity, StatusBar } from 'react-native';
+import { useNavigation , useRoute} from '@react-navigation/native';
+
+function fundOverviewPage (props) {
+
+
+}
